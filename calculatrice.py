c5b49366d30de1f5f14a25514b7d83bb00f7411c
root <root@localhost.localdomain>
2017-01-02 14:36:00 +0100

    add calc

---
+++ calculatrice.py
@@ -0,0 +1,12 @@
+#####################################################################
+#@ calculatrice
+#@ 01/02/2017
+#@ Jerome THEMEE
+#####################################################################
+# Module Import
+
+# function and variable creation
+
+# body script
+
+entreeUn = raw_input("entrez le nombre N 1")
